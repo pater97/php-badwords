7bff007c192bc805f5a2cfc53f1f30f4089fab9f
pater97 <giuseppepater997@gmail.com>
2021-12-01 16:00:52 +0100

    esercizio completato

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+$swords=$_GET['swords'];
+$asterisc="***";
 $paragrafo =
- "What’s important is the fucking drive to see a project through no matter what.
+ "What’s important is the fucking  drive to see a project through no matter what.
  Form follows fucking function. Accomplishment validates belief, and belief inspires accomplishment.
  Don’t fucking censor yourself. Sterility leads to susceptibility. 
  If you fucking give up, you will achieve nothing.
@@ -10,6 +12,25 @@ $paragrafo =
  then one must expect and accept the fucking possibility of failure.Fuck.
  You need to sit down and sketch more fucking ideas because stalking your ex on facebook isn’t going to get you anywhere.";
 
- echo $paragrafo;
  var_dump(strlen($paragrafo));
+ var_dump($swords);
+ $newParag=str_replace($swords,$asterisc,$paragrafo);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <p>
+     <?php 
+     echo $newParag;
+     ?>
+  </p>
+</body>
+</html>
