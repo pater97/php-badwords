c794e4556be4793db804a8c86afd0b3ba3fc4b47
pater97 <giuseppepater997@gmail.com>
2021-12-01 15:01:24 +0100

    paragrafo e lunghezza stampati correttamente

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$paragrafo =
+ "What’s important is the fucking drive to see a project through no matter what.
+ Form follows fucking function. Accomplishment validates belief, and belief inspires accomplishment.
+ Don’t fucking censor yourself. Sterility leads to susceptibility. 
+ If you fucking give up, you will achieve nothing.
+ Widows and orphans are terrible fucking tragedies, both in real life and definitely in typography.
+ There is no right fucking answer. The splendor of the mystery is that you don’t understand.
+ Saul Bass on failure: Failure is built into creativity… the creative act involves this element of ‘newness’ and ‘experimentalism,’
+ then one must expect and accept the fucking possibility of failure.Fuck.
+ You need to sit down and sketch more fucking ideas because stalking your ex on facebook isn’t going to get you anywhere.";
+
+ echo $paragrafo;
+ var_dump(strlen($paragrafo));
+?>
